92903e7565e5b91d4c68911e1a5d821cf441cb19
Oleg Gaidukov <ogaidukov@gmail.com>
2018-05-08 11:30:11 +0300

    well tested escrow smart contract

--- contracts/Et4Token.sol
+++ contracts/Et4Token.sol
@@ -5,18 +5,103 @@ import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
 
 contract ET4Token is MintableToken, BurnableToken {
-    using SafeMath for uint;
+    using SafeMath for uint256;
 
     string public symbol;
     string public  name;
     uint8 public decimals;
+
+    /* Numerator and denominator of common fraction.
+    E.g. 1 & 25 mean one twenty fifths, i.e. 0.04 = 4% */
+    uint256 public escrow_fee_numerator; /* 1 */
+    uint256 public escrow_fee_denominator; /* 25 */
+
+    struct EscrowElement {
+      bool exists;
+      address src;
+      address dst;
+      uint256 value;
+    }
+    /* escrow_id => EscrowElement struct */
+    mapping (bytes20 => EscrowElement) escrows;
+
+    event EscrowStarted(
+      bytes20 indexed escrow_id,
+      EscrowElement escrow_element
+    );
+
+    event EscrowReleased(
+      bytes20 indexed escrow_id,
+      EscrowElement escrow_element
+    );
+
+    event EscrowCancelled(
+      bytes20 indexed escrow_id,
+      EscrowElement escrow_element
+    );
 
     constructor(uint256 _initialAmount) public {
       symbol = "ET4";
       name = "Eticket4 Token";
       decimals = 18;
       totalSupply_ = _initialAmount;
+      escrow_fee_numerator = 1;
+      escrow_fee_denominator = 25;
+
       balances[owner] = totalSupply_;
-      /* emit Transfer(address(0), owner, totalSupply_); */
+      emit Transfer(address(0), owner, totalSupply_);
+    }
+
+    function startEscrow(bytes20 escrow_id, address to, uint256 value) public returns (bool) {
+      require(to != address(0));
+      require(value <= balances[msg.sender]);
+      require(escrows[escrow_id].exists != true);
+
+      balances[msg.sender] = balances[msg.sender].sub(value);
+      EscrowElement memory escrow_element = EscrowElement(true, msg.sender, to, value);
+      escrows[escrow_id] = escrow_element;
+
+      emit EscrowStarted(escrow_id, escrow_element);
+
+      return true;
+    }
+
+    function releaseEscrow(bytes20 escrow_id, address fee_destination) public returns (bool) {
+      require(fee_destination != address(0));
+      require(escrows[escrow_id].exists == true);
+
+      EscrowElement storage escrow_element = escrows[escrow_id];
+
+      /* uint256 value = escrow_element.value * ((100 - escrow_fee) / 100);
+      uint256 fee = escrow_element.value * (escrow_fee / 100); */
+      uint256 fee = escrow_element.value.mul(escrow_fee_numerator).div(escrow_fee_denominator);
+      uint256 value = escrow_element.value.sub(fee);
+
+      require(value == 192);
+
+      balances[escrow_element.dst] = balances[escrow_element.dst].add(value);
+      balances[fee_destination] = balances[fee_destination].add(fee);
+
+      /* Workaround because of lack of feature. See https://github.com/ethereum/solidity/issues/3577 */
+      EscrowElement memory _escrow_element = escrow_element;
+      emit EscrowReleased(escrow_id, _escrow_element);
+
+      delete escrows[escrow_id];
+
+      return true;
+    }
+
+    function cancelEscrow(bytes20 escrow_id) public returns (bool) {
+      EscrowElement storage escrow_element = escrows[escrow_id];
+
+      balances[escrow_element.src] = balances[escrow_element.src].add(escrow_element.value);
+
+      /* Workaround because of lack of feature. See https://github.com/ethereum/solidity/issues/3577 */
+      EscrowElement memory _escrow_element = escrow_element;
+      emit EscrowCancelled(escrow_id, _escrow_element);
+
+      delete escrows[escrow_id];
+
+      return true;
     }
 }
--- test/TestEt4Token.js
+++ test/TestEt4Token.js
@@ -1,10 +1,57 @@
 var Et4Token = artifacts.require("Et4Token");
 
-contract('Et4Token', function(accounts) {
-  it("should put 10000000 MetaCoin in total supply", function() {
-    return Et4Token.deployed().then(function(instance) {
-      return instance.totalSupply.call();
-    }).then(function(balance) {
-      assert.equal(balance.valueOf(), 10000000, "10000000 wasn't supplied in total");
-    });
-  })});
+contract('Et4Token test', async (accounts) => {
+  var account1 = accounts[1];
+  var account2 = accounts[2];
+  var account3 = accounts[3];
+  var account4 = accounts[4];
+  var account5 = accounts[5];
+  var account6 = accounts[6];
+  var account7 = accounts[7];
+  var account8 = accounts[8];
+  var account9 = accounts[9];
+
+  it("should put 10000000 ET4 coins in total supply", async () => {
+    let instance = await Et4Token.deployed();
+
+    let balance = await instance.totalSupply.call();
+    assert.equal(balance.valueOf(), 10000000, "10000000 wasn't supplied in total");
+  });
+
+  it("should put 20000 ET4 tokens on account2", async () => {
+    let instance = await Et4Token.deployed();
+
+    await instance.transfer(account2, 20000);
+
+    let balance = await instance.balanceOf.call(account2);
+    assert.equal(balance.valueOf(), 20000, "20000 wasn't put");
+  });
+
+  it("should put 200 ET4 tokens from account2 to escrow in favor of address3 then release escrow", async () => {
+    let instance = await Et4Token.deployed();
+
+    await instance.startEscrow('1234567', account3, 200, {from: account2});
+    let balance2 = await instance.balanceOf.call(account2);
+    assert.equal(balance2.valueOf(), 19800, "balance of a source account2 must be exactly 19800");
+
+    await instance.releaseEscrow('1234567', account9);
+    let balance3 = await instance.balanceOf.call(account3);
+    assert.equal(balance3.valueOf(), 192, "balance of a target account3 must be exactly 192");
+    let balance9 = await instance.balanceOf.call(account9);
+    assert.equal(balance9.valueOf(), 8, "balance of a fee account9 must be exactly 8");
+  });
+
+  it("should put 400 ET4 tokens from account2 to escrow in favor of address4 then cancel escrow", async () => {
+    let instance = await Et4Token.deployed();
+
+    await instance.startEscrow('7654321', account4, 400, {from: account2});
+    let balance2 = await instance.balanceOf.call(account2);
+    assert.equal(balance2.valueOf(), 19400, "balance of a source account2 must be exactly 19400");
+
+    await instance.cancelEscrow('7654321');
+    balance2 = await instance.balanceOf.call(account2);
+    assert.equal(balance2.valueOf(), 19800, "balance of a source account2 must be exactly 19800");
+    let balance4 = await instance.balanceOf.call(account4);
+    assert.equal(balance4.valueOf(), 0, "balance of a fee account4 must be exactly 0");
+  });
+});
