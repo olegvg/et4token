678742e1c849332bde89cf0a57d0d13da4a7d626
Oleg Gaidukov <ogaidukov@gmail.com>
2018-05-22 12:45:30 +0300

    production (rinkeby) truffle config added

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    live: {
+      host: "127.0.0.1", // via ssh tunnel
+      port: 48545,
+      network_id: 4 // rinkeby
     }
   }
 };
